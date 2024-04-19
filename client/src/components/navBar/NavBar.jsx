620729c063e74f357fb411fa1918ed45950d249e
Matias <mati.dehoyosmdp@gmail.com>
2024-04-19 15:19:08 -0300

    responsive

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import style from './NavBar.module.css'
 import { FaFile } from "react-icons/fa";
+import { FiMenu } from 'react-icons/fi';
 
 
 const NavBar = () => {
-   
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleShowMenu = () => {
+        setIsVisible(!isVisible);
+      }
+    
+      const ocultarMenu = () => {
+        const menu = document.querySelector(`.${style.menu}`);
+        menu.style.left = "-800px";
+      }
 
     return(
         <div className={style.container}>
             <a href="#"><img src='img.png'/></a>
-            <div className={style.menu}>
+            <div className={style.menu} onClick={ocultarMenu} style={isVisible ? { left: '0px' } : { left: '-800px' }} >
                 <nav>
                  <ul>
                     <li><a href="#">Home</a></li>
@@ -20,6 +31,9 @@ const NavBar = () => {
             </div>
             <div className={style.cv}>
             <a href="https://drive.google.com/file/d/1t2_vNEdiFrSU70cCBypvux1nOaXq32wg/view?usp=drive_link" target="_blank" rel="noopener noreferrer"><button className={style.cv}>Download cv <FaFile className={style.file}/></button></a>
+            </div>
+            <div className={style.menuResp}>
+              <button className={style.menuButton} onClick={handleShowMenu}><FiMenu /></button>
             </div>
         </div>
     )
--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -1,6 +1,5 @@
 import style from './Projects.module.css'
 
-
 const Projects = () => {
    
 
@@ -13,21 +12,24 @@ const Projects = () => {
                                     <a href="https://electroauto.vercel.app" target='_blank'>
                                         <img src="electroauto.png" alt="electroauto" />
                                         <h4>Electroauto</h4>
-                                        <p>Responsive aplicación web ecommerce. Con varias vistas, incluyendo tienda online, y panel autoadministrable.</p>
+                                        <p className={style.desc}>Aplicación web ecommerce. Con varias vistas, incluyendo tienda online, y panel autoadministrable.</p>
+                                        <p className={style.tecno}>NodeJs, Express, PostgreSql, Sequelize. <br/>ReactJs, Redux, Css3.<br/>Mercado Pago. Auth0.</p>
                                     </a>
                                     </div>
                                     <div className={style.project}>
                                     <a href="https://wedevelop.vercel.app" target='_blank'>
                                         <img src="wedev.png" alt="wedeveloper" />
                                         <h4>WeDevelop</h4>
-                                        <p>Aplicación web autocotizadora. Con varias vistas, incluyendo un cuestionario a traves del cual se autocotiza el producto. Con panel autoadministrable.</p>
+                                        <p className={style.desc}>Aplicación web autocotizadora. Con varias vistas, incluyendo un cuestionario a traves del cual se autocotiza el producto. Con panel autoadministrable.</p>
+                                        <p className={style.tecno}>NodeJs, Express, MongoDB, Mongoose. <br/>ReactJs, Redux, Css3.<br/>Mercado Pago. Auth0.</p>
                                     </a>
                                     </div>
                                     <div className={style.project}>
                                     <a href="https://matiasdehoyos.vercel.app" target='_blank'>
                                         <img src="portfolio.png" alt="portfolio" />
                                         <h4>Portfolio</h4>
-                                        <p>Página web que muestra información relevante, preoyectos y contacto de un profesional.</p>
+                                        <p className={style.desc}>One page que muestra información relevante, preoyectos y contacto de un profesional.</p>
+                                        <p className={style.tecno}>ReactJs, Html5, Css3.</p>
                                     </a>
                                     </div>
                                 </div>
