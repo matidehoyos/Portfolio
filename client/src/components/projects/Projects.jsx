1e5cdc6e69adfa674195d98c8f34b8f1e1f8d26b
Matias <mati.dehoyosmdp@gmail.com>
2024-04-25 09:31:43 -0300

    new project

--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -25,6 +25,14 @@ const Projects = () => {
                                     </a>
                                     </div>
                                     <div className={style.project}>
+                                    <a href="https://www.mododigital.tech" target='_blank'>
+                                        <img src="md.png" alt="mododigital" />
+                                        <h4>Modo digital</h4>
+                                        <p className={style.desc}>Landing page. Institucional, reune toda la información relevante de la empresa, y muestra su servicio.</p>
+                                        <p className={style.tecno}>Html, Css, Javascript</p>
+                                    </a>
+                                    </div>
+                                    <div className={style.project}>
                                     <a href="https://matiasdehoyos.vercel.app" target='_blank'>
                                         <img src="portfolio.png" alt="portfolio" />
                                         <h4>Portfolio</h4>
