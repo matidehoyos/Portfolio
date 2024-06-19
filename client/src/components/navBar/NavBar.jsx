217e18679674f0a40b3c65bb0c8cbeea7a5a6935
Matias <mati.dehoyosmdp@gmail.com>
2024-06-19 16:15:32 -0300

    }

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -30,7 +30,7 @@ const NavBar = () => {
                 </nav>
             </div>
             <div className={style.cv}>
-            <a href="https://drive.google.com/file/d/1xH8GQhvoWL1M4Y73Pjkjuqr9dgMZfFOz/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button className={style.cv}>Download CV <FaFile className={style.file}/></button></a>
+            <a href="https://drive.google.com/file/d/1mrwKU6KvZJQTvRNWpyN25lUVCMW4jVDu/view" target="_blank" rel="noopener noreferrer"><button className={style.cv}>Download CV <FaFile className={style.file}/></button></a>
             </div>
             <div className={style.menuResp}>
               <button className={style.menuButton} onClick={handleShowMenu}><FiMenu /></button>
