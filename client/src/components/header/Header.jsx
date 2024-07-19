904e4cb3f245e862d15a9de4e5bd00ec251368c5
Matias <mati.dehoyosmdp@gmail.com>
2024-07-19 10:38:14 -0300

    }

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,38 +5,48 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import { MdDescription } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
     useEffect(() => {
-        AOS.init(); 
-      }, []);
+        AOS.init();
+    }, []);
 
-    return(
+    return (
         <div className={style.container}>
-                        <div className={style.imgPre} data-aos="fade-up" data-aos-duration="800">
-                                <img src="yo.png" alt="foto mia" />
-                        </div>
+            <div className={style.imgPre} data-aos="fade-up" data-aos-duration="800">
+                <img src="yo.png" alt="foto mia" />
+            </div>
 
-                        <div className={style.txtPre} data-aos="fade-up" data-aos-duration="800">
-                                <p className={style.mati}>I'm Matias De Hoyos</p>
-                                <h1><span className={style.im}></span>Full Stack - <br/>FrontEnd Developer</h1>
-                                <p className={style.des}>Web developer with a passion for creating visually stunning and user-friendly websites.</p>               
-                                <div className={style.cv}>
-                                    <a href="https://drive.google.com/file/d/1sYzgf98QuJlBUoD-sYAcf2UbSA2C5A3i/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV <MdDescription className={style.file}/></a>
-                                </div>
-                                <div className={style.redes}>
-                                    <nav>
-                                        <ul>
-                                            <li><a href='https://www.linkedin.com/in/matias-de-hoyos-802bb4212/' target='_blank'><FaLinkedin /></a></li>
-                                            <li><a href='https://github.com/matidehoyos' target='_blank'><AiFillGithub /></a></li>
-                                            <li><a target="_blank" href='https://www.instagram.com/matiasdehoyos/'><AiFillInstagram /></a></li>
-                                        </ul>
-                                    </nav>
-                                </div>
-                        </div>
+            <div className={style.txtPre} data-aos="fade-up" data-aos-duration="800">
+                <p className={style.mati}>I'm Matias De Hoyos</p>
+                <h1><span className={style.im}></span>Full Stack<br />FrontEnd Developer</h1>
+                <p className={style.des}>Web developer with a passion for creating visually stunning and user-friendly websites.</p>
+                <div className={style.cv}>
+                    <a href="https://drive.google.com/file/d/1sYzgf98QuJlBUoD-sYAcf2UbSA2C5A3i/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download CV <MdDescription className={style.file} /></a>
                 </div>
-           )
+                <div className={style.menu} >
+                    <nav>
+                        <ul>
+                            <li><Link to="/about">About</Link></li>
+                            <li><Link to="/projects">Projects</Link></li>
+                            <li><Link to="/contact">Contact</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className={style.redes}>
+                    <nav>
+                        <ul>
+                            <li><a href='https://www.linkedin.com/in/matias-de-hoyos-802bb4212/' target='_blank'><FaLinkedin /></a></li>
+                            <li><a href='https://github.com/matidehoyos' target='_blank'><AiFillGithub /></a></li>
+                            <li><a target="_blank" href='https://www.instagram.com/matiasdehoyos/'><AiFillInstagram /></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header;
