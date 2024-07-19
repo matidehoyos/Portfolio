34e7b78188e0457ba38b483bdf887a8b642a829a
Matias <mati.dehoyosmdp@gmail.com>
2024-07-19 16:24:19 -0300

    }

--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -29,6 +29,15 @@ const Projects = () => {
                             </div>
                     ))}
                 </div>
+                <div className={style.menu}  >
+                                <nav>
+                                    <ul>
+                                        <li><Link to="/">Home</Link></li>
+                                        <li><Link to="/about">About</Link></li>
+                                        <li><Link to="/contact">Contact</Link></li>
+                                    </ul>
+                                </nav>
+                          </div>
         </div>
     )
 }
